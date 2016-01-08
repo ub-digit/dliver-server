10bd61d9a431efe5fa05efcd338c7046edaa9aca
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-01-08 10:06:29 +0100

    Update rake script to reflect new server structure

--- rake_script.sh
+++ rake_script.sh
@@ -3,7 +3,7 @@
 PATH=$PATH:/usr/local/bin
 . /usr/local/lib/rvm
 
-DIR=/data/$2/dLiver/current
+DIR=/apps/dliver-server/current
 cd $DIR
 rvm use 2.1.5
-RAILS_ENV=$3 rake "$1"
+RAILS_ENV=$2 bundle exec rake "$1"
